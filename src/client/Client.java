f4834b569f7ebf7b30469fea620f24e6fe0b5ffe
PLLIEHO <mgribov259@gmail.com>
2022-06-01 08:21:20 +0300

    Initial commit

---
+++ src/client/Client.java
@@ -0,0 +1,57 @@
+package client;
+
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+
+public class Client {
+    private final DatagramSocket socket = new DatagramSocket();
+    private InetAddress address;
+    public static int PORT;
+
+    public Client() throws UnknownHostException, SocketException {
+    }
+
+    public static void main(String[] args) throws IOException {
+        try{
+            Client client = new Client();
+            client.start();
+        } catch (SocketException | UnknownHostException e) {
+            System.out.println("Невозможно подключиться к серверу.");
+            System.exit(1);
+        }
+    }
+    public void start() throws IOException {
+        System.out.println("Введите IP адрес сервера...");
+        String addressStr = InputClaimer.input();
+        if(!addressStr.equals("localhost")){
+            try {
+                long checkAddr = Long.parseLong(addressStr);
+            } catch (NumberFormatException e){
+                System.out.println("Адрес введен неверно. Повторите ввод.");
+                start();
+            }
+        }
+        address = InetAddress.getByName(addressStr);
+        try {
+            System.out.println("Введите порт...");
+            String port = InputClaimer.input();
+            if(Integer.parseInt(port)<65535&&Integer.parseInt(port)>0){
+                Client.PORT = Integer.parseInt(port);
+            } else{
+                System.out.println("Порт введен неверно. Повторите ввод.");
+                start();
+            }
+        } catch (NumberFormatException p){
+            System.out.println("Порт введен неверно. Повторите ввод.");
+            start();
+        }
+
+        CommandChecker checker = new CommandChecker(address, socket);
+        checker.check();
+    }
+}
---
+++ src/client/Sender.java
@@ -0,0 +1,65 @@
+package client;
+
+
+
+import common.Answer;
+import common.Pack;
+import common.Request;
+import common.Serializer;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+
+
+public class Sender {
+    public boolean exitFlag = false;
+
+    public void send(byte[] byteOutput, InetAddress address, DatagramSocket socket) {
+
+        DatagramPacket toServer = new DatagramPacket(byteOutput, byteOutput.length, address, Client.PORT);
+        DatagramPacket packetIn = new DatagramPacket(new byte[Serializer.SIZE], Serializer.SIZE);
+        try {
+
+                System.out.println("Пакет отправлен, ждите.");
+                for(int i = 0; i<11; i++){
+                    try {
+                        socket.setSoTimeout(0);
+                        socket.send(toServer);
+                        socket.setSoTimeout(1000);
+                        socket.receive(packetIn);
+
+                        System.out.println("Пакет доставлен.");
+                        Answer answer = (Answer) new Serializer().deserialize(packetIn.getData());
+
+                        Serializer serializer = new Serializer();
+                        byte[] byteApproval = serializer.serialize(new Request(null, new Pack(null, approvalCreator(toServer)))).toByteArray();
+                        socket.send(new DatagramPacket(byteApproval, byteApproval.length, address, Client.PORT));
+                        System.out.println("Подтверждение отправлено.");
+                        System.out.println(answer.getStr());
+                        break;
+                    } catch (SocketTimeoutException e){
+                        if(i!=10){
+                        continue;}
+                        else{
+                            socket.close();
+                            if(exitFlag){
+                                System.exit(0);
+                            }
+                            System.out.println("Сервер недоступен. Попробуйте ввести другой порт и IP.");
+                            Client client = new Client();
+                            client.start();
+                        }
+                    }
+                }
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    private String approvalCreator(DatagramPacket toServer){
+        return toServer.getAddress().getHostAddress();
+    }
+}
